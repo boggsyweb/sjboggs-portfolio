58dcb3ccd5320ec0bc7371562a153a9e965fde0f
bosspigg <123977427+boggsyweb@users.noreply.github.com>
2023-07-07 16:40:24 +0100

    style: profile projects UI changed in desktop

--- data.js
+++ data.js
@@ -3,29 +3,29 @@ export const projects = [
 {
     name: 'Boolebots',
     tech: 'React TS',
-    image: './images/projects/boolebots.png',
-    description: 'This project was part of <a href="https://www.chingu.io/" target="_blank" >Chingu</a> voyage 44 and was my first time working with a team. Collaborating with a group with vastly different communication styles and skillsets was equal parts challenging and fun. We utilized Agile methodology to structure the workflow. For my part, I designed the overall theme with Figma, did most of the CSS, and wrote the code for some key elements, such as the modals, the loader, and a color-coding feature. I also made smaller contributions to other parts of the project.',
-    links: '<a href="https://github.com/boggsyweb/v44-tier2-team-27" target="_blank">Code</a> <a href="https://gentle-semifreddo-84c257.netlify.app/" target="blank">Live-View</a>'
+    image: './images/projects/boolebots.webp',
+    description: 'This project was part of <a href="https://www.chingu.io/" target="_blank" >Chingu</a> voyage 44 and was my first time working with a team. Collaborating with a group with vastly different communication styles and skillsets was equal parts challenging and fun. We utilized Agile methodology to structure the workflow. For my part, I designed the overall theme with Figma, did most of the CSS, and wrote the code for some key elements, such as the modals, the loader, and a color-coding feature. I also collaborated with team members for other parts of the project.',
+    links: '<a href="https://github.com/boggsyweb/v44-tier2-team-27" target="_blank">Code /</a> <a href="https://gentle-semifreddo-84c257.netlify.app/" target="blank">Live-View</a>'
 },
 {
     name: 'Mercado Biológico',
     tech: 'JavaScript',
-    image: './images/projects/mercado.png',
+    image: './images/projects/mercado.webp',
     description: 'The local farmers market in my adopted hometown of Setúbal has no real web presence. I volunteered my time to build them a website where they could reach a wider customer base, as well as keep their current customers up to date. Part of this project entails not only collaborating with the farmers and vendors, but also the local government that supervises the activities of the market. The website is a work in progress, as we gain final approval from all entities involved.',
-    links: '<a href="https://github.com/boggsyweb/Mercado" target="_blank">Code</a> <a href="https://stalwart-kleicha-5c8fb6.netlify.app" target="_blank">Live-View</a>'
+    links: '<a href="https://github.com/boggsyweb/Mercado" target="_blank">Code / </a> <a href="https://stalwart-kleicha-5c8fb6.netlify.app" target="_blank">Live-View</a>'
 },
 {
     name: 'Javascript Quiz',
     tech: 'JavaScript Typed.js',
-    image: './images/projects/quiz.png',
+    image: './images/projects/quiz.webp',
     description: 'I needed a way to help me get over the hurdle of memorizing some key aspects of vanilla JavaScript, so I created this quiz. Through creating the questions and answers, as well as writing the code, I was able to solidify my learning to that point. I styled it to mimic a retro computer screen, but added a font toggle for those who do not care for a pixelized font. Typed.js was used to run the typewriter effect.',
-    links: '<a href="https://github.com/boggsyweb/multi-quiz" target="_blank">Code</a> <a href="https://stunning-faloodeh-cc82f2.netlify.app" target="_blank">Live-View</a>'
+    links: '<a href="https://github.com/boggsyweb/multi-quiz" target="_blank">Code / </a> <a href="https://stunning-faloodeh-cc82f2.netlify.app" target="_blank">Live-View</a>'
 },
 {
     name: 'Design Work',
     tech: 'Figma Gimp Inkscape',
-    image: './images/projects/controlpanel.png',
-    description: 'I use Figma to make all of my wireframes and prototypes for my projects. When I cannot find the assets I want, I make them myself. Here are some examples of assets I have created and shared to the Figma community, some of which have been copied numerous times by other users.',
+    image: './images/projects/controlpanel.webp',
+    description: 'I use Figma for all wireframes and prototypes, and some design elements. When assets are not available in Figma, I create them myself to share with the community. I use Photoshop and Gimp for image creation and manipulation. Here are some examples of my work.',
     links: '<a href="https://dribbble.com/sjboggs" target="_blank">Dribble</a>'
 },
 ]
