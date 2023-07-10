408dc5d8c7132dd406ef7d5f108f8c8afe728f1b
bosspigg <123977427+boggsyweb@users.noreply.github.com>
2023-07-10 18:58:41 +0100

    refactor: improved mobile nav

--- index.js
+++ index.js
@@ -1,65 +1,96 @@
 import { projects } from "./data.js"
 
-window.addEventListener('scroll', function() {
-  const header = document.querySelectorAll('.header');
+// mobile nav button open/close
+
+const mobileToggle = document.querySelector('.mobile-toggle');
+const mobileNav = document.getElementById('mobile-nav');
+
+  mobileToggle.addEventListener('click', () => {
+  mobileToggle.classList.toggle('change');
+  mobileNav.classList.toggle('change');
+  document.body.classList.toggle('no-scroll');
+
+});
+
+const links = document.querySelectorAll('.nav li a');
+links.forEach((link) => {
+  link.addEventListener('click', () => {
+    mobileNav.classList.remove('change');
+    mobileToggle.classList.remove('change');
+    document.body.classList.remove('no-scroll');
+
+  });
+});
+
+window.addEventListener('scroll', () => {
+  const headerElements = document.querySelectorAll('.header');
   const sectionContent = document.querySelector('.section-content');
   const sections = document.querySelectorAll('section');
-  const desktopNav = document.getElementById('desktop-nav')
-  const menuToggle = document.getElementById('menuToggle')
+  const desktopNav = document.getElementById('desktop-nav');
+  const burgerBar = document.getElementById('burger-bar')
 
-  const headerHeight = header[0].offsetHeight; 
 
+  const headerHeight = headerElements[0].offsetHeight;
   const scrollThreshold = sectionContent.offsetTop - headerHeight;
 
-  header.forEach(function(header) {
-    if (window.scrollY > scrollThreshold) {
+  headerElements.forEach((header) => {
+    if (window.scrollY >= scrollThreshold) {
       header.classList.add('visible');
       const currentSection = getCurrentSection(sections, headerHeight);
       const backgroundColor = getComputedStyle(currentSection).backgroundColor;
       header.style.backgroundColor = backgroundColor;
 
-
       const textColor = getContrastColor(backgroundColor);
       header.style.color = textColor;
 
+      mobileNav.style.backgroundColor = backgroundColor;
+
       const svgs = header.querySelectorAll('svg');
-      svgs.forEach(function(svg) {
+      svgs.forEach((svg) => {
         svg.style.fill = textColor;
       });
+
       desktopNav.style.color = textColor;
-      
+
       const navLinks = desktopNav.querySelectorAll('a');
-      navLinks.forEach(function(link) {
+      navLinks.forEach((link) => {
         link.style.color = textColor;
       });
 
-      const Hamburger = menuToggle.querySelectorAll('span');
-      Hamburger.forEach(function(span) {
-        span.style.background = textColor;
-      })
+      const mobileLinks = mobileNav.querySelectorAll('a');
+      mobileLinks.forEach((link) => {
+        link.style.color = textColor;
+      });
 
+      const hamburger = burgerBar.querySelectorAll('.bar');
+      hamburger.forEach((span) => {
+        span.style.background = textColor;
+      });
     } else {
       header.classList.remove('visible');
       header.style.backgroundColor = '';
       header.style.color = '';
 
       const svgs = header.querySelectorAll('svg');
-      svgs.forEach(function(svg) {
+      svgs.forEach((svg) => {
         svg.style.fill = '';
       });
+
       desktopNav.style.color = '';
-      
+
       const navLinks = desktopNav.querySelectorAll('a');
-      navLinks.forEach(function(link) {
+      navLinks.forEach((link) => {
         link.style.color = '';
       });
-      const Hamburger = menuToggle.querySelectorAll('span');
-      Hamburger.forEach(function(span) {
+
+      const hamburger = burgerBar.querySelectorAll('.bar');
+      hamburger.forEach((span) => {
         span.style.background = '';
-      })
+      });
     }
   });
 });
+
 
 function getCurrentSection(sections, headerHeight) {
   let currentSection = sections[0];
@@ -74,6 +105,7 @@ function getCurrentSection(sections, headerHeight) {
   return currentSection;
 }
 
+// text and SVG colors related to backgrond colors
 function getContrastColor(backgroundColor) {
  
   const threshold = 150; 
@@ -82,28 +114,24 @@ function getContrastColor(backgroundColor) {
   return brightness > threshold ? '#232A79' : '#FDF3F6'; 
 }
 
-
+// projects 
 const projectWrapper = document.querySelector('.project-wrapper')
 
-function displayProjects() {
-  let projectDisplay = ''
-  for (let project of projects) {
-    projectDisplay += `
-    <div class='projects'>
+const projectDisplay = projects.map((project) => `
+  <div class='projects'>
     <h3 class='project-title'>${project.name}</h3>
     <!--<h5 class='project-tech'>${project.tech}</h5>-->
     <img class='project-img' src='${project.image}' alt='${project.alt}'>
     <p class='project-description'>${project.description}</p>
     <h4 class='project-links'>${project.links}</h4>
-    </div>`
-  }
+  </div>
+`).join('');
 
-projectWrapper.innerHTML = projectDisplay
-}
-displayProjects()
+projectWrapper.innerHTML = projectDisplay;
 
 
 
-// **automatically updates year in footer (based on user's computer)**
+
+// automatically updates year in footer (based on user's computer)
 let date = (new Date()).getFullYear()
-document.getElementById('year').innerHTML = date
+document.getElementById('year').textContent = date
