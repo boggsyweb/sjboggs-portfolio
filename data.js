c5c34f857382fff0bbeea343902b75eed29d27bf
bosspigg <123977427+boggsyweb@users.noreply.github.com>
2023-07-12 19:59:49 +0100

    refactor: updated links for projects

--- data.js
+++ data.js
@@ -5,7 +5,7 @@ export const projects = [
     tech: 'React TS',
     image: './images/projects/boolebots.webp',
     alt: 'screenshots of Boolebots in desktop and mobile views',
-    description: 'This project was part of <a href="https://www.chingu.io/" target="_blank" >Chingu</a> voyage 44. We had six weeks to deploy and submit a completed project, while adhering to an explicit set of requirements. This was my first time working with a team. The experience of collaborating with a group with vastly different communication styles and skillsets was equal parts challenging and fun. We utilized Agile methodology to structure the workflow, which helped it run smoothly.',
+    description: 'This project was part of <a href="https://www.chingu.io/" target="_blank" >Chingu</a> voyage 44. We had six weeks to deploy and submit a completed project, while adhering to an explicit set of requirements. This was my first time working with a team. The experience of collaborating with a group of developers with vastly different communication styles and skillsets was equal parts challenging and fun. We utilized Agile methodology to structure the workflow, which helped it run smoothly.',
     links: '<a href="https://github.com/boggsyweb/v44-tier2-team-27" target="_blank">Code /</a> <a href="https://gentle-semifreddo-84c257.netlify.app/" target="blank">Live-View</a>'
 },
 {
@@ -14,7 +14,7 @@ export const projects = [
     image: './images/projects/mercado.webp',
     alt: 'screenshots of Mercado Biologico in desktop and mobile views',
     description: 'The local farmers market in my adopted hometown of Setúbal has no real web presence. I volunteered my time to build them a website where they could reach a wider customer base, as well as keep their current customers up to date. Part of this project entails not only collaborating with the farmers and vendors, but also the local government that supervises the activities of the market. The website is a work in progress, as we gain final approval from all entities involved.',
-    links: '<a href="https://github.com/boggsyweb/Mercado" target="_blank">Code / </a> <a href="https://stalwart-kleicha-5c8fb6.netlify.app" target="_blank">Live-View</a>'
+    links: '<a href="https://github.com/boggsyweb/Mercado" target="_blank">Code / </a> <a href="https://mercado-five.vercel.app/" target="_blank">Live-View</a>'
 },
 {
     name: 'Javascript Quiz',
@@ -22,7 +22,7 @@ export const projects = [
     image: './images/projects/quiz.webp',
     alt: 'screenshots of Javascript Quiz App in desktop and mobile views',
     description: 'I needed a way to help me get over the hurdle of memorizing some key aspects of vanilla JavaScript, so I created this quiz. Through creating the questions and answers, as well as writing the code, I was able to solidify my learning to that point. I styled it to mimic a retro computer screen, but added a font toggle for those who do not care for a pixelized font. Typed.js was used to run the typewriter effect.',
-    links: '<a href="https://github.com/boggsyweb/multi-quiz" target="_blank">Code / </a> <a href="https://stunning-faloodeh-cc82f2.netlify.app" target="_blank">Live-View</a>'
+    links: '<a href="https://github.com/boggsyweb/multi-quiz" target="_blank">Code / </a> <a href="https://multi-quiz.vercel.app/" target="_blank">Live-View</a>'
 },
 {
     name: 'Design Work',
